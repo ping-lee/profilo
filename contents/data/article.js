143f45c834a1475310fb4f277121f824b8f8cc20
alfred <59156256+ping-lee@users.noreply.github.com>
2020-11-18 10:54:32 +0800

    daily english

---
+++ contents/data/article.js
@@ -0,0 +1,38 @@
+import { Text } from "@chakra-ui/core"
+import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser'
+export default [
+    {
+        title: '',
+        paragraph: [
+            {
+                content: `<Text>It is come up with scenarios in which legislators or judges engineer a second Trump term and plunge the nation into a constitutional crisis. But even with a conservative Supreme Court, a Republican-led Senate and Republican-led congressional delegations in most states, election law experts are confident this won’t happen. </Text>`,
+                translate: '议员或法官们会密谋策划特朗普连任，让整个国家陷入宪法危机，这种情况也不是不可能。然而，选举法专家们相信，即便有一个保守派最高法院、一个由共和党领导的参议院，并且大多数州的州议会也由共和党领导，这种情况依然不会发生。',
+            },
+            {
+                content: 'A primary tactic of the Trump campaign and conservative groups has been to use lawsuits and other maneuvers to try to prevent states from certifying their results. The idea is, in part, that if election officials can’t certify Mr. Biden’s victories in time, Republican-controlled state legislatures could step in and name pro-Trump electors.',
+                translate: '特朗普竞选团队和保守派团体的一个主要策略就是利用诉讼和其他手段，试图阻止各州认证选举结果。这个想法从某种程度上就是，如果选举官员不能及时证明拜登获胜，共和党控制的州议会则可以介入，提名支持特朗普的选举团成员。',
+            },
+            {
+                content: 'In Pennsylvania and Michigan, Republicans have filed lawsuits to block certification based on unsubstantiated allegations of voting or counting irregularities. But judges have roundly rejected the Trump campaign’s complaints so far.',
+                translate: '在宾夕法尼亚州和密歇根州，共和党人已提起诉讼，以未经证实的投票或计票违规指控为由阻止认证。但到目前为止，法官们全面驳回了特朗普竞选团队的申诉。',
+            },
+            {
+                content: 'Even if the counts were certified on time, state legislators could still, in theory, go rogue and appoint pro-Trump electoral slates to compete with the slates certified by governors. But Pennsylvania Republicans have already said they won’t do that, and even if they or legislators in another state did, it probably wouldn’t survive legal scrutiny.',
+                translate: '即使计票按时得到确认，理论上，州议员仍然可以向州长确认的选举人团发起竞争，耍流氓地任命支持特朗普的选举团成员。但宾夕法尼亚州共和党人已表示，他们不会这么做，即便他们或其他州的议员这么做了，可能也无法通过法律审查。',
+            },
+            {
+                content: 'On Dec. 14, each state’s electors will formally cast their votes. Most states have laws requiring electors to vote for the candidate they were pledged to, limiting the possibility of “faithless electors.”',
+                translate: '12月14日，各州选举人团将正式投票。大多数州的法律都要求选举人团投票给他们许诺的候选人，从而限制了“失信选举人团”的可能性。',
+            },
+        ], 
+    },
+    {
+        title: '',
+        paragraph: [
+            {
+                content: '',
+                translate: '',
+            },
+        ], 
+    },
+]

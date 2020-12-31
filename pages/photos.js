7e502ea37770da1e2d5c3913cf8acea1708522f7
alfred <59156256+ping-lee@users.noreply.github.com>
2020-12-31 11:20:53 +0800

    add photo

--- pages/photos.js
+++ pages/photos.js
@@ -1,7 +1,87 @@
 import { Box } from '@chakra-ui/core'
 import Header from "@components/header"
 import SEO from "@components/seo"
+import Gallery from 'react-grid-gallery'
 import Footer from "@components/footer"
+
+
+const IMAGES =
+[{
+        src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
+        thumbnail: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_n.jpg",
+        caption: "After Rain (Jeshu John - designerspics.com)"
+},
+{
+        src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
+        thumbnail: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
+        tags: [{value: "Ocean", title: "Ocean"}, {value: "People", title: "People"}],
+        caption: "Boats (Jeshu John - designerspics.com)"
+},
+{
+  src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
+  thumbnail: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
+  tags: [{value: "Ocean", title: "Ocean"}, {value: "People", title: "People"}],
+  caption: "Boats (Jeshu John - designerspics.com)"
+},
+{
+  src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
+  thumbnail: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
+  tags: [{value: "Ocean", title: "Ocean"}, {value: "People", title: "People"}],
+  caption: "Boats (Jeshu John - designerspics.com)"
+},
+{
+  src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
+  thumbnail: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
+  tags: [{value: "Ocean", title: "Ocean"}, {value: "People", title: "People"}],
+  caption: "Boats (Jeshu John - designerspics.com)"
+},
+
+{
+  src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
+  thumbnail: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
+  tags: [{value: "Ocean", title: "Ocean"}, {value: "People", title: "People"}],
+  caption: "Boats (Jeshu John - designerspics.com)"
+},
+{
+  src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
+  thumbnail: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
+  tags: [{value: "Ocean", title: "Ocean"}, {value: "People", title: "People"}],
+  caption: "Boats (Jeshu John - designerspics.com)"
+},
+{
+  src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
+  thumbnail: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
+  tags: [{value: "Ocean", title: "Ocean"}, {value: "People", title: "People"}],
+  caption: "Boats (Jeshu John - designerspics.com)"
+},
+{
+  src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
+  thumbnail: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
+  tags: [{value: "Ocean", title: "Ocean"}, {value: "People", title: "People"}],
+  caption: "Boats (Jeshu John - designerspics.com)"
+},
+{
+  src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
+  thumbnail: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
+  tags: [{value: "Ocean", title: "Ocean"}, {value: "People", title: "People"}],
+  caption: "Boats (Jeshu John - designerspics.com)"
+},
+{
+  src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
+  thumbnail: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
+  tags: [{value: "Ocean", title: "Ocean"}, {value: "People", title: "People"}],
+  caption: "Boats (Jeshu John - designerspics.com)"
+},
+{
+  src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
+  thumbnail: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
+  tags: [{value: "Ocean", title: "Ocean"}, {value: "People", title: "People"}],
+  caption: "Boats (Jeshu John - designerspics.com)"
+},
+{
+        src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
+        thumbnail: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg",
+}]
 
 function PhotosPage({}) {
   return (
@@ -12,9 +92,10 @@ function PhotosPage({}) {
       />
       <Header fixed="fixed"/>
       <Box pt={3} mt="4.5rem">
-        kkk
+        <Gallery enableImageSelection={false} images={IMAGES}/>
+        
       </Box>
-      <Footer />
+      
     </>
   )
 }
